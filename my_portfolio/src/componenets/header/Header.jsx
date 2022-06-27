fe38b80bea50b7116eef0ede4462f37e765e51b7
olusegun Owolabi <olujawo1996@gmail.com>
2022-06-27 04:28:56 +0100

    Header

--- my_portfolio/src/componenets/header/Header.jsx
+++ my_portfolio/src/componenets/header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/my_picture.jpg'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+
+        <h5> Hello I'm</h5>
+        <h1>Olusegun Owolabi</h1>
+          <h5 className="text-light">FrontEnd Developer</h5> 
+          <CTA/>
+
+          <div className="me">
+            <img src= {ME} alt="me"/>
+          </div>
+      </div>
+    </header>
   )
-}
+} 
 
 export default Header
